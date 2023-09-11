44869b03144f6e592d0208cb1f69304a3e742b9b
Abdullah NUR <abdullahnur.ybs@gmail.com>
2023-09-11 15:26:49 +0300

    Feat: userDefaults sistem added

--- MealPlanner/ListingView.swift
+++ MealPlanner/ListingView.swift
@@ -1,4 +1,3 @@
-
 import SwiftUI
 import Foundation
 
@@ -15,7 +14,6 @@ struct ListingView: View {
     public func calculateAverageCalories() -> String{
         let totalCalories = foodList.reduce(0) { $0 + $1.caloryType }
         calorie = Double(totalCalories) / Double(foodList.count)
-        print("Ortalama Kalori:", calorie)
         resultText = ""
         if calorie < 0.7 {
             resultText = "Düşük düzeyde kalorili bir liste."
@@ -26,10 +24,10 @@ struct ListingView: View {
         }else{
             resultText = "Liste boş yemek yeme vakti."
         }
-            
+        
         return resultText
-
-        }
+        
+    }
     
     
     
@@ -48,40 +46,82 @@ struct ListingView: View {
         default:
             return ""
         }
-
+        
     }
     private func formatDate(date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM - HH:mm"
         return dateFormatter.string(from: date)
         
-
+        
     }
     
-    private  func sortFoodListByDate(){
-        isAsceding = !isAsceding
-       
+    private let sortingPreferenceKey = "sortingPreference"
+    
+    private let userDefaults = UserDefaults.standard
+    
+    
+    
+    func saveSortingPreference(sortKey: [String] ) {
+        userDefaults.set(sortKey, forKey: sortingPreferenceKey)
+    }
+    
+    func getSortingPreference() {
+        
+        let array : [String]  = userDefaults.stringArray(forKey: sortingPreferenceKey) ?? [String]()
+        
+        if array.count > 1 {
+            
+            isAsceding = Bool(array[1]) ?? false
+            
+            switch array[0]{
+                
+            case "name":
+                sortFoodListByName(isCurrentAsceding: isAsceding)
+                break
+            case "date":
+                sortFoodListByDate(isCurrentAsceding: isAsceding)
+                break
+            case "calory":
+                sortFoodListByCaloryType(isCurrentAsceding: isAsceding)
+        
+            default:
+                break
+            }
+            
+        }
+    }
+    
+    
+    
+    private  func sortFoodListByDate(isCurrentAsceding: Bool){
+        isAsceding = isCurrentAsceding
+        
         if !isAsceding {
             foodList = foodList.sorted(by:{$1.time < $0.time})
         }
         else {
             foodList = foodList.sorted(by:{$1.time > $0.time})
         }
+        
+        saveSortingPreference(sortKey: ["date",String(isAsceding)])
+        
     }
     
-    private func sortFoodListByName(){
-       isAsceding = !isAsceding
-
+    private func sortFoodListByName(isCurrentAsceding:Bool){
+        isAsceding = isCurrentAsceding
+        
         if !isAsceding {
             foodList = foodList.sorted(by:{$0.foodName < $1.foodName})
         }
         else {
             foodList = foodList.sorted(by:{$0.foodName > $1.foodName})
         }
-      
+        saveSortingPreference(sortKey: ["name",String(isAsceding)])
+        
     }
-    private func sortFoodListByCaloryType(){
-       isAsceding = !isAsceding
+    private func sortFoodListByCaloryType(isCurrentAsceding:Bool){
+        isAsceding = isCurrentAsceding
         
         if !isAsceding {
             foodList = foodList.sorted(by:{$0.caloryType < $1.caloryType})
@@ -89,26 +129,30 @@ struct ListingView: View {
         else {
             foodList = foodList.sorted(by:{$0.caloryType > $1.caloryType})
         }
+        saveSortingPreference(sortKey: ["calory",String(isAsceding)])
+        
     }
     
     private func deleteItem(at offsets: IndexSet) {
-           foodList.remove(atOffsets: offsets)
-            dataModel.foodList.remove(atOffsets: offsets)
-       }
+        foodList.remove(atOffsets: offsets)
+        dataModel.foodList.remove(atOffsets: offsets)
+        
+    }
     private func resetList(){
         foodList = dataModel.foodList;
+        
     }
-   
+    
     
     func performSearch(with newValue: String) {
-
+        
         if newValue.isEmpty {
             foodList = dataModel.foodList
         }
         else {
             foodList = dataModel.foodList.filter({$0.foodName.localizedCaseInsensitiveContains(newValue)})
         }
-     }
+    }
     
     
     var body: some View {
@@ -131,7 +175,7 @@ struct ListingView: View {
                                 .font(.system(size: 12))
                             Spacer()
                             Text("\(caloryCheck(erc:item.caloryType))")
-                           
+                            
                         }
                     }
                     
@@ -141,57 +185,55 @@ struct ListingView: View {
             .onAppear{
                 foodList = dataModel.foodList
                 calculateAverageCalories()
-
+                getSortingPreference()
             }.toolbar {
                 EditButton()
                 Menu("Sırala"){
                     Button {
-                        sortFoodListByName()
+                        sortFoodListByName(isCurrentAsceding: !isAsceding)
                     } label: {
                         Label("Ad", systemImage: "textformat.alt" )
                     }
                     Button {
-                        sortFoodListByDate()
+                        sortFoodListByDate(isCurrentAsceding: !isAsceding)
                     } label: {
                         Label("Tarih", systemImage: "arrow.up.and.down.text.horizontal" )
                     }
                     Button {
-                        sortFoodListByCaloryType()
+                        sortFoodListByCaloryType(isCurrentAsceding: !isAsceding)
                     } label: {
                         Label("Kalori", systemImage: "greaterthan.circle.fill" )
                     }
-                     Button {
+                    Button {
                         resetList()
                     } label: {
                         Label("Reset", systemImage: "gobackward" )
                     }
                     
                 }
-               
-
+                
+                
             }
             .listStyle(.plain)
-                .navigationTitle("Kalori Kontrol")
-                .listStyle(InsetGroupedListStyle())
-                .environment(\.horizontalSizeClass, .compact)
-                .searchable(text: $searchTerm)
-                .onChange(of: searchTerm) { newValue in
-                    performSearch(with: newValue)
-                       
+            .navigationTitle("Kalori Kontrol")
+            .listStyle(InsetGroupedListStyle())
+            .environment(\.horizontalSizeClass, .compact)
+            .searchable(text: $searchTerm)
+            .onChange(of: searchTerm) { newValue in
+                performSearch(with: newValue)
+                
             }
         }
         
     }
     
 }
- 
-    
-    
-    struct ListingView_Previews: PreviewProvider {
-        static var previews: some View {
-            ListingView()
-            
-        }
+
+
+
+struct ListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingView()
+        
     }
-
-
+}
