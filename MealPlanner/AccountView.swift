592f7eaa68c1bc830a2c0171e70c180a31e001cc
Abdullah NUR <abdullahnur.ybs@gmail.com>
2023-10-06 15:53:13 +0300

    Feat: New Account view

---
+++ MealPlanner/AccountView.swift
@@ -0,0 +1,293 @@
+import SwiftUI
+
+struct ToastView: View {
+    let message: String
+    let iconName: String
+    @Binding var showToastMessage: Bool
+
+    var body: some View {
+        ZStack {
+            VStack {
+                Image(systemName: iconName)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.orange)
+                    .padding(.bottom, 10)
+                
+                Text(message)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+            .background(Color.gray.opacity(0.5))
+            .cornerRadius(20)
+            .transition(.opacity)
+            .animation(.easeInOut(duration: 0.4))
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    showToastMessage = false
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct AccountView: View {
+    @EnvironmentObject private var dataModel: DataModel
+    @State private var isLoggingOut = false
+    @State private var showingDeleteAccountPopup = false
+    @State private var showToastMessage = false
+
+    func deleteUser() {
+        showToastMessage = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            showToastMessage = false
+            dataModel.loginMyArray.removeAll()
+            dataModel.isLogin = false
+        }
+    }
+
+    func logoutUser() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            dataModel.isLogin = false
+        }
+    }
+
+    func appSetting() {
+    }
+
+    struct ChangePasswordView: View {
+        @Binding var oldPassword: String
+        @State private var newPassword: String = ""
+        @State private var newPasswordConfirm: String = ""
+        @State private var isPasswordMatchError = false
+        @State private var isOldPasswordVisible = false
+        @State private var isNewPasswordVisible = false
+        @State private var isNewConfirmPasswordVisible = false
+        @FocusState private var isFocusedOldPassword: Bool
+        @FocusState private var isFocusedNewPassword: Bool
+        @FocusState private var isFocusedNewConfirmPassword: Bool
+        @State private var showToastMessage = false
+        @Environment(\.dismiss) var dismiss
+
+        var isPasswordMatch: Bool {
+            return newPassword == newPasswordConfirm
+        }
+
+        var buttonCheck: Bool {
+            return !oldPassword.isEmpty && !newPassword.isEmpty && !newPasswordConfirm.isEmpty
+        }
+
+        func changePasswordButton() {
+            isFocusedOldPassword = false
+            isFocusedNewPassword = false
+            isFocusedNewConfirmPassword = false
+
+            if isPasswordMatch {
+                isPasswordMatchError = false
+                newPassword = ""
+                newPasswordConfirm = ""
+                oldPassword = ""
+                showToastMessage = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    showToastMessage = false
+                }
+            } else {
+                isPasswordMatchError = true
+                Text("Yeni şifreler eşleşmiyor.")
+                    .foregroundColor(.red)
+                    .padding(.top, 5)
+                    .padding(.bottom, 10)
+            }
+        }
+
+        var body: some View {
+            VStack {
+                ZStack {
+                    if isOldPasswordVisible {
+                        TextField("Eski Şifre", text: $oldPassword)
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .padding(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(isFocusedOldPassword ? Color.orange : Color.gray.opacity(0.2), lineWidth: 2)
+                                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                            )
+                            .focused($isFocusedOldPassword)
+                    } else {
+                        SecureField("Eski Şifre", text: $oldPassword)
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .padding(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(isFocusedOldPassword ? Color.orange : Color.gray.opacity(0.2), lineWidth: 2)
+                                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                            )
+                            .focused($isFocusedOldPassword)
+                    }
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            isOldPasswordVisible.toggle()
+                            isFocusedNewPassword = false
+                            isFocusedNewConfirmPassword = false
+                        }) {
+                            Image(systemName: isOldPasswordVisible ? "eye.slash" : "eye")
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.trailing, 15)
+                        .buttonStyle(BorderedButtonStyle())
+                    }
+                }
+
+
+                if isPasswordMatchError {
+                    Text("Yeni şifreler eşleşmiyor.")
+                        .foregroundColor(.red)
+                        .padding(.top, 5)
+                        .padding(.bottom, 10)
+                } else {
+                    Text("")
+                }
+
+                Button(action: changePasswordButton) {
+                    HStack {
+                        Text("Şifreyi Değiştir")
+                            .foregroundColor(.white)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .background(NavigationLink("", destination: ContentView()))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(buttonCheck ? Color.orange : Color.gray)
+                    .cornerRadius(10)
+                }
+                .disabled(!buttonCheck)
+
+                Spacer()
+                if showToastMessage {
+                    ToastView(message: "Şifre başarılı bir şekilde değiştirildi", iconName: "hand.thumbsup", showToastMessage: $showToastMessage)
+                        .transition(.opacity)
+                        .animation(.easeInOut(duration: 0.4))
+                }
+            }
+            .padding()
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        dismiss()
+                        oldPassword = ""
+                        newPasswordConfirm = ""
+                        newPassword = ""
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+
+                            Text("Geri Dön")
+                            Spacer()
+                        }
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+
+    var body: some View {
+        ZStack {
+            VStack(alignment: .leading, spacing: 20) {
+                NavigationStack {
+                    List {
+                        Section {
+                            HStack {
+                                Text("Şifreyi Değiştir")
+
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .foregroundColor((showingDeleteAccountPopup || isLoggingOut) ? Color.gray.opacity(0.3) : .orange)
+                            .overlay {
+                                // NavigationLink(destination: {changePassword( oldPassword : $password ) }, label: { EmptyView() })
+                                // .opacity(0)
+                            }
+
+                            Button(action: {
+                                self.showingDeleteAccountPopup = true
+                                isLoggingOut = true
+                            }) {
+                                HStack {
+                                    Text("Hesabı Sil")
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                }
+                            }
+                            .alert(isPresented: $showingDeleteAccountPopup) {
+                                Alert(
+                                    title: Text("Hesabı Sil"),
+                                    message: Text("Hesabınızı Silmek İstediğinizden Emin'misiniz"),
+                                    primaryButton: .destructive(Text("Evet")) {
+                                        deleteUser()
+                                    },
+                                    secondaryButton: .cancel(Text("Hayır")) {
+                                        self.showingDeleteAccountPopup = false
+                                        isLoggingOut = false
+                                    }
+                                )
+                            }
+
+                            Button(action: logoutUser) {
+                                HStack {
+                                    Text("Çıkış Yap")
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                }
+                            }
+                        }
+                        Section {
+                            Button(action: appSetting) {
+                                HStack {
+                                    Text("Ayarlar")
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                }
+                            }
+                            Button(action: appSetting) {
+                                HStack {
+                                    Text("Uygulama Hakkında")
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                }
+                            }
+                        }
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            HStack {
+                                Image(systemName: "person")
+                                Text(dataModel.loginMyArray.first ?? "")
+                                    .font(.system(size: 25))
+                                Spacer()
+                            }
+                            .padding(.top, 25)
+                        }
+                    }
+                    .listStyle(.insetGrouped)
+                }
+            }
+
+            if showToastMessage {
+                ToastView(message: "Hesap başarılı bir şekilde silindi", iconName: "hand.thumbsup", showToastMessage: $showToastMessage)
+                    .transition(.opacity)
+                    .animation(.easeInOut(duration: 0.4))
+            }
+        }
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
